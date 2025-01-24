32142a9549647fe41123f5544fc117c0120afe46
Sehee Jang <seheejang94@gmail.com>
2025-01-24 17:57:35 +0900

    add: Assignment 3 Directions - Task 3

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,14 @@
+// exports.triggerError = (req, res, next) => {
+//   const error = new Error("Intentional Server Error Triggered");
+//   error.status = 500;
+//   throw error; // This will be caught by the error handler middleware
+// };
+
 exports.triggerError = (req, res, next) => {
-  const error = new Error("Intentional Server Error Triggered");
-  error.status = 500;
-  throw error; // This will be caught by the error handler middleware
+  try {
+    throw new Error("Intentional Server Error Triggered");
+  } catch (err) {
+    err.status = 500;
+    next(err);
+  }
 };
